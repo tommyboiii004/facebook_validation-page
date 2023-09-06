bb39d05486fe3db06985f359a0f4b5b8029d6012
adrieljoshua <adrieljoshua.25cs@licet.ac.in>
2023-09-06 19:28:51 +0530

    completed user validation

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// have to update
+function validateData() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // Mock user data
+    var dataBase = {
+        "users": [
+            {
+                "email": "user1@gmail.com",
+                "password": "password"
+            },
+            {
+                "email": "user2@example.com",
+                "password": "password"
+            },
+            {
+                "email": "user3@example.com",
+                "password": "password"
+            }
+        ]
+    };
+
+    
+    var userFound = dataBase.users.some(function(user) {
+        return user.email === email && user.password === password;
+    });
+
+    if (!userFound) {
+        alert("Invalid Email and password!!!");
+    } else {
+        alert("Valid email and password");
+    }
+}
